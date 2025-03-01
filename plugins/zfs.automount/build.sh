f67358b46eb5506f82c42ab600096243b389147b
KarlKKang <karl_kai.kang@icloud.com>
2025-02-28 20:11:18 -0500

    Migrate zfs-automount to PHP

--- plugins/zfs.automount/build.sh
+++ plugins/zfs.automount/build.sh
@@ -6,7 +6,7 @@ ver=$(date +%Y.%m.%d)
 cd "$(dirname "$0")"
 
 find source/event -type f -exec chmod +x {} \;
-tar -cJf zfs.encryption.automount-$ver.txz -C source .
+tar -cJf zfs.automount-$ver.txz -C source .
 
 echo "Version: $ver"
-echo "md5sum: $(md5sum zfs.encryption.automount-$ver.txz)"
+echo "md5sum: $(md5sum zfs.automount-$ver.txz)"
---
+++ plugins/zfs.automount/source/zfs-mount.php
@@ -0,0 +1,183 @@
+<?php
+
+$zvol_mounts = [
+    'kokorowatari/system/docker' => '/mnt/kokorowatari/system/docker',
+];
+
+enum LOG_LEVEL: string
+{
+    case INFO = 'notice';
+    case WARNING = 'warning';
+    case ERROR = 'error';
+
+    public function unraid_level(): string
+    {
+        return match ($this) {
+            self::INFO => 'normal',
+            self::WARNING => 'warning',
+            self::ERROR => 'alert',
+        };
+    }
+}
+
+function logger(string $message, LOG_LEVEL $level = LOG_LEVEL::INFO): void
+{
+    if ($level !== LOG_LEVEL::INFO) {
+        try {
+            exec('/usr/local/emhttp/webGui/scripts/notify -e "ZFS Automount" -d ' . escapeshellarg($message) . ' -i ' . $level->unraid_level());
+        } catch (ValueError $e) {
+            echo 'Failed to send notification: ' . $e->getMessage() . PHP_EOL;
+        }
+    }
+    $message = '[' . $level->value . '] ' . $message;
+    try {
+        exec('logger -t zfs-automount ' . escapeshellarg($message));
+    } catch (ValueError $e) {
+        echo 'Failed to log message: ' . $e->getMessage() . PHP_EOL;
+    }
+}
+
+function system_command(string $command, array &$output = []): bool
+{
+    $command .= ' 2>&1';
+    try {
+        $result = exec($command, $output, $retval);
+    } catch (ValueError $e) {
+        logger($command, LOG_LEVEL::ERROR);
+        logger('Failed to execute system command: ' . $e->getMessage(), LOG_LEVEL::ERROR);
+        return false;
+    }
+    if ($retval !== 0 || $result === false) {
+        logger($command, LOG_LEVEL::ERROR);
+        logger('Command failed with exit code ' . $retval, LOG_LEVEL::ERROR);
+        foreach ($output as $line) {
+            logger($line, LOG_LEVEL::ERROR);
+        }
+        return false;
+    }
+    return true;
+}
+
+function get_encrypted_datasets(): array|false
+{
+    $datasets = [];
+    $output = [];
+    // Not tested: sorting by mountpoint allows mounting of parent datasets before children, so that children
+    //  can have their own encryption root.
+    system_command('zfs list -t filesystem,volume -H -o name,keylocation,mountpoint -s mountpoint', $output);
+    foreach ($output as $line_str) {
+        $line = explode("\t", $line_str);
+        if (count($line) !== 3) {
+            logger('Failed to parse ZFS list output: ' . $line_str, LOG_LEVEL::ERROR);
+            return false;
+        }
+        [$name, $keylocation, $mountpoint] = $line;
+        if ($keylocation === 'prompt' || $keylocation === 'none') {
+            continue;
+        }
+        if ($mountpoint === '-') {
+            $mountpoint = null;
+        }
+        $datasets[$name] = $mountpoint;
+    }
+    return $datasets;
+}
+
+function create_mountpoint(string $mountpoint): bool
+{
+    if (!is_dir($mountpoint) && !mkdir($mountpoint, 0777, true)) {
+        logger('Failed to create mountpoint: ' . $mountpoint, LOG_LEVEL::ERROR);
+        return false;
+    }
+    if (!system_command('chattr +i ' . escapeshellarg($mountpoint))) {
+        logger('Failed to set mountpoint immutable: ' . $mountpoint, LOG_LEVEL::ERROR);
+        return false;
+    }
+    return true;
+}
+
+function mount(string $dataset, string|null $mountpoint): bool
+{
+    if ($mountpoint !== null) {
+        if (!create_mountpoint($mountpoint)) {
+            return false;
+        }
+    }
+    if (!system_command('zfs load-key ' . escapeshellarg($dataset))) {
+        logger('Failed to load key for dataset: ' . $dataset, LOG_LEVEL::ERROR);
+        return false;
+    }
+    if ($mountpoint === null) {
+        logger('Loaded key for dataset: ' . $dataset);
+        return true;
+    }
+    if (!system_command('zfs mount -R ' . escapeshellarg($dataset))) {
+        logger('Failed to mount dataset: ' . $dataset, LOG_LEVEL::ERROR);
+        return false;
+    }
+    logger('Mounted dataset: ' . $dataset . ' at ' . $mountpoint);
+    return true;
+}
+
+function mount_zvol(string $dataset, string $mountpoint): bool
+{
+    if (!create_mountpoint($mountpoint)) {
+        return false;
+    }
+    if (!system_command('mount ' . escapeshellarg('/dev/zvol/' . $dataset) . ' ' . escapeshellarg($mountpoint))) {
+        logger('Failed to mount ZFS volume: ' . $dataset, LOG_LEVEL::ERROR);
+        return false;
+    }
+    logger('Mounted ZFS volume: ' . $dataset . ' at ' . $mountpoint);
+    return true;
+}
+
+function unload_all_keys(): void
+{
+    if (!system_command('zfs unload-key -a')) {
+        logger('Failed to unload all keys', LOG_LEVEL::ERROR);
+        return;
+    }
+    logger('All keys unloaded');
+}
+
+function unmount_zvol(string $mountpoint): void
+{
+    if (!system_command('umount ' . escapeshellarg($mountpoint))) {
+        logger('Failed to unmount ZFS volume: ' . $mountpoint, LOG_LEVEL::ERROR);
+        return;
+    }
+    logger('Unmounted ZFS volume: ' . $mountpoint);
+}
+
+function main(array $argv, array $zvol_mounts): void
+{
+    if (count($argv) !== 2) {
+        logger('Invalid number of arguments', LOG_LEVEL::ERROR);
+        return;
+    }
+    $action = $argv[1];
+    if ($action === 'mount') {
+        $datasets = get_encrypted_datasets();
+        if ($datasets === false) {
+            return;
+        }
+        foreach ($datasets as $dataset => $mountpoint) {
+            mount($dataset, $mountpoint);
+        }
+        foreach ($zvol_mounts as $dataset => $mountpoint) {
+            mount_zvol($dataset, $mountpoint);
+        }
+    } elseif ($action === 'unmount_zvols') {
+        foreach ($zvol_mounts as $mountpoint) {
+            unmount_zvol($mountpoint);
+        }
+    } elseif ($action === 'unload_keys') {
+        unload_all_keys();
+    } else {
+        logger('Invalid action: ' . $action, LOG_LEVEL::ERROR);
+    }
+
+}
+
+main($argv, $zvol_mounts);
