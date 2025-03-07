3dfd12a07b3483965a7720c546e174480b4ce843
KarlKKang <karl_kai.kang@icloud.com>
2025-03-07 16:07:27 -0500

    Update the plugin build script

--- plugins/kissshot-plugin/build.sh
+++ plugins/kissshot-plugin/build.sh
@@ -6,7 +6,8 @@ ver=$(date +%Y.%m.%d)
 cd "$(dirname "$0")"
 
 find source/event -type f -exec chmod +x {} \;
-tar -cJf zfs.automount-$ver.txz -C source .
+find source -type f -name "*.sh" -exec chmod +x {} \;
+tar -cJf kissshot-plugin-$ver.txz -C source .
 
 echo "Version: $ver"
-echo "md5sum: $(md5sum zfs.automount-$ver.txz)"
+echo "md5sum: $(md5sum kissshot-plugin-$ver.txz)"
