9db146226426d56b15c7227f70a83c922bb977b7
KarlKKang <karl_kai.kang@icloud.com>
2025-03-07 15:57:46 -0500

    Add a shell script to trigger the recycle-clean PHP script

---
+++ plugins/kissshot-plugin/source/recycle-clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+php "$SCRIPT_DIR/recycle-clean.php"
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    logger -t recycle-clean "[error] Script exited with error code $exit_code"
+    /usr/local/emhttp/webGui/scripts/notify -e "Recycle Clean" -d "Script exited with error code $exit_code" -i "alert"
+fi
