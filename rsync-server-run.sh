fa31041df268b32e48d8558258fbe489a2e9eb7b
KarlKKang <karl_kai.kang@icloud.com>
2025-03-10 03:05:27 -0400

    Always restart the rsync server

--- rsync-server-run.sh
+++ rsync-server-run.sh
@@ -6,7 +6,7 @@ cd /mnt/user/system/rsync-server-source
 
 docker build -t rsync-server .
 
-docker run -d --name kjhybb-nas-rsync-server -p 8022:22 \
+docker run -d --name kjhybb-nas-rsync-server -p 8022:22 --restart=always \
 -v /mnt/user/kjhybb-nas:/data \
 -v /mnt/user/appdata/kjhybb-nas-rsync-server:/config:ro \
 -e PASSWORD_FILE=/config/password \
