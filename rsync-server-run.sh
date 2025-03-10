1701b71b36c946aad61fb44e86cc1e95479b6aae
KarlKKang <karl_kai.kang@icloud.com>
2025-03-10 02:53:02 -0400

    Set up a rsync server for Synology backup

---
+++ rsync-server-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+cd /mnt/user/system/rsync-server-source
+
+docker build -t rsync-server .
+
+docker run -d --name kjhybb-nas-rsync-server -p 8022:22 \
+-v /mnt/user/kjhybb-nas:/data \
+-v /mnt/user/appdata/kjhybb-nas-rsync-server:/config:ro \
+-e PASSWORD_FILE=/config/password \
+rsync-server
