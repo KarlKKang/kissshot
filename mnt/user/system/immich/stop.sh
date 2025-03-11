a1e0c29428bd9b0333ab30980810daff30f1ecc0
KarlKKang <karl_kai.kang@icloud.com>
2025-03-11 15:28:19 -0400

    Add script to start and stop the immich docker

---
+++ mnt/user/system/immich/stop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"
+
+docker run --rm -it \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker compose down
