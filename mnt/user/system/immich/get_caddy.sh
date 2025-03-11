745e116741f6e0f3a439d5d6e3f55eab44a1289b
KarlKKang <karl_kai.kang@icloud.com>
2025-03-11 14:39:16 -0400

    Add caddy reverse proxy for immich

---
+++ mnt/user/system/immich/get_caddy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"
+
+curl -L -o caddy 'https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fcaddy-dns%2Fcloudflare&idempotency=60693466075896'
