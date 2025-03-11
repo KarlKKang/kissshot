8a37dde48bbae45fac7971b7867ac7ab3128476e
KarlKKang <karl_kai.kang@icloud.com>
2025-03-11 16:47:39 -0400

    Use a single script for immich docker management

--- mnt/user/system/immich/docker-compose.sh
+++ mnt/user/system/immich/docker-compose.sh
@@ -6,6 +6,6 @@ cd "$(dirname "$0")"
 docker run --rm -it \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /mnt/user/appdata/immich:/mnt/user/appdata/immich:ro \
-    -v "$PWD:$PWD":ro \
+    -v "$PWD:$PWD:ro" \
     -w="$PWD" \
-    docker compose up -d
+    docker compose "$@"
