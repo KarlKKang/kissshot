c6c80f48416b6c6dbbf0fcb4c0b9bdb9a1e02f14
KarlKKang <karl_kai.kang@icloud.com>
2025-03-11 15:28:08 -0400

    Make caddy executable

--- mnt/user/system/immich/get_caddy.sh
+++ mnt/user/system/immich/get_caddy.sh
@@ -4,3 +4,4 @@ set -e
 cd "$(dirname "$0")"
 
 curl -L -o caddy 'https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fcaddy-dns%2Fcloudflare&idempotency=60693466075896'
+chmod +x caddy
