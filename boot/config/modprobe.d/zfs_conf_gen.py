60717932e8f06770c26f4f4aa93dd374a140ece9
KarlKKang <karl_kai.kang@icloud.com>
2025-03-21 04:11:08 -0400

    Always write the `zfs_multilist_num_sublists` option

--- boot/config/modprobe.d/zfs_conf_gen.py
+++ boot/config/modprobe.d/zfs_conf_gen.py
@@ -11,12 +11,14 @@ percent_available_cpus = available_cpus / total_cpus
 script_dir = os.path.dirname(os.path.realpath(__file__))
 zfs_conf_path = os.path.join(script_dir, 'zfs.conf')
 with open(zfs_conf_path, 'w') as f:
+    # The defaults are accurate as of ZFS 2.3.1
+    # https://openzfs.github.io/openzfs-docs/man/v2.3/4/zfs.4.html
+
     # default: 50
     f.write(f'options zfs metaslab_preload_pct={round(50 * percent_available_cpus)}\n')
 
     # default: max(number of online CPUs, 4)
-    if available_cpus > 4:
-        f.write(f'options zfs zfs_multilist_num_sublists={available_cpus}\n')
+    f.write(f'options zfs zfs_multilist_num_sublists={max(available_cpus, 4)}\n')
 
     # default: 100
     f.write(f'options zfs zfs_zil_clean_taskq_nthr_pct={round(100 * percent_available_cpus)}\n')
