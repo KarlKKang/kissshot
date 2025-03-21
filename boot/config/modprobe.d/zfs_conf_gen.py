111518ab4c82938ff6822a5ad85f571f8fe1c140
KarlKKang <karl_kai.kang@icloud.com>
2025-03-21 03:39:45 -0400

    Seems like ZFS 2.3 increased `arc_max` on Linux to match FreeBSD. Let's remove all custom memory settings and see how ZFS handles memory by itself.

--- boot/config/modprobe.d/zfs_conf_gen.py
+++ boot/config/modprobe.d/zfs_conf_gen.py
@@ -6,46 +6,11 @@ available_cpus = 28
 total_cpus = 64
 l2arc_tbw = 2400
 
-total_available_ram = total_ram - total_reserved_ram
-percent_available_ram = total_available_ram / total_ram
 percent_available_cpus = available_cpus / total_cpus
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 zfs_conf_path = os.path.join(script_dir, 'zfs.conf')
 with open(zfs_conf_path, 'w') as f:
-    # default: 10 (10% of system memory)
-    f.write(f'options zfs zfs_arc_lotsfree_percent={round(10 * percent_available_ram)}\n')
-
-    # default: max(1/2 of system memory, 67108864)
-    #  customized to 75% of system memory
-    zfs_arc_max = round(total_available_ram * 0.75)
-    if zfs_arc_max > 67108864:
-        f.write(f'options zfs zfs_arc_max={zfs_arc_max}\n')
-
-    # default: max(1/32 of system memory, 33554432)
-    zfs_arc_min = round(total_available_ram / 32)
-    if zfs_arc_min > 33554432:
-        f.write(f'options zfs zfs_arc_min={zfs_arc_min}\n')
-
-    # default: max(1/64 of system memory, 512 KiB)
-    zfs_arc_sys_free = round(total_available_ram / 64)
-    if zfs_arc_sys_free > 512 * 1024:
-        f.write(f'options zfs zfs_arc_sys_free={zfs_arc_sys_free}\n')
-
-    # default: min(1/4 of system memory, 4 GiB)
-    zfs_dirty_data_max_max_cap = 4 * 1024 * 1024 * 1024
-    zfs_dirty_data_max_max = min(round(total_available_ram / 4), zfs_dirty_data_max_max_cap)
-    if zfs_dirty_data_max_max < zfs_dirty_data_max_max_cap:
-        f.write(f'options zfs zfs_dirty_data_max_max={zfs_dirty_data_max_max}\n')
-
-    # default: 10% of system memory, capped at `zfs_dirty_data_max_max`
-    zfs_dirty_data_max = round(0.1 * total_available_ram)
-    if zfs_dirty_data_max < zfs_dirty_data_max_max:
-        f.write(f'options zfs zfs_dirty_data_max={round(0.1 * total_available_ram)}\n')
-
-    # default: 20 (i.e. 1/20 of system memory)
-    f.write(f'options zfs zfs_scan_mem_lim_fact={round(20 * 1 / percent_available_ram)}\n')
-
     # default: max(number of online CPUs, 4)
     if available_cpus > 4:
         f.write(f'options zfs zfs_multilist_num_sublists={available_cpus}\n')
