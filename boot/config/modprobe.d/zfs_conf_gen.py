687a4395798116207f44c372c673140e5d154119
KarlKKang <karl_kai.kang@icloud.com>
2025-03-21 03:55:38 -0400

    Add two additional ZFS parameters that controls CPU usage

--- boot/config/modprobe.d/zfs_conf_gen.py
+++ boot/config/modprobe.d/zfs_conf_gen.py
@@ -11,6 +11,9 @@ percent_available_cpus = available_cpus / total_cpus
 script_dir = os.path.dirname(os.path.realpath(__file__))
 zfs_conf_path = os.path.join(script_dir, 'zfs.conf')
 with open(zfs_conf_path, 'w') as f:
+    # default: 50
+    f.write(f'options zfs metaslab_preload_pct={round(50 * percent_available_cpus)}\n')
+
     # default: max(number of online CPUs, 4)
     if available_cpus > 4:
         f.write(f'options zfs zfs_multilist_num_sublists={available_cpus}\n')
@@ -28,6 +31,9 @@ with open(zfs_conf_path, 'w') as f:
 
     # default: 80
     f.write(f'options zfs zio_taskq_batch_pct={round(80 * percent_available_cpus)}\n')
+
+    # default: max(number of online CPUs, 32)
+    f.write(f'options zfs zvol_threads={max(available_cpus, 32)}\n')
 
     # default: 0
     f.write('options zfs l2arc_exclude_special=1\n')
