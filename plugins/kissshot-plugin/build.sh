dc3cd5f3fa71d39f8753eb2aeab576c554917bdf
KarlKKang <karl_kai.kang@icloud.com>
2025-04-06 16:22:10 -0400

    Copy the plugin archive instead of moving them when installing

--- plugins/kissshot-plugin/build.sh
+++ plugins/kissshot-plugin/build.sh
@@ -5,6 +5,8 @@ set -e
 ver=$(date +%Y.%m.%d)
 cd "$(dirname "$0")"
 
+rm -f kissshot-plugin-*.txz
+
 find source/event -type f -exec chmod +x {} \;
 find source -type f -name "*.sh" -exec chmod +x {} \;
 tar -cJf kissshot-plugin-$ver.txz -C source .
--- plugins/kissshot-plugin/install.sh
+++ plugins/kissshot-plugin/install.sh
@@ -5,6 +5,6 @@ set -e
 cd "$(dirname "$0")"
 
 mkdir -p /boot/config/plugins/kissshot-plugin
-mv ./*.txz /boot/config/plugins/kissshot-plugin
+cp ./*.txz /boot/config/plugins/kissshot-plugin
 
 plugin install "$(realpath ./kissshot-plugin.plg)" forced
