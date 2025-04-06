82e461fbe1cf36120a6ef15e1f1ed1e1c44dad8a
KarlKKang <karl_kai.kang@icloud.com>
2025-04-06 16:19:04 -0400

    Allow only one running TRIM job

--- plugins/kissshot-plugin/source/trim.php
+++ plugins/kissshot-plugin/source/trim.php
@@ -15,8 +15,27 @@ $zpools = [
     'kokorowatari',
 ];
 
+const RUNTIME_DIR = '/mnt/user/system/trim';
+const RUNTIME_FILE = 'lock';
+
+class RuntimeState extends RuntimeStateTemplate
+{
+    public function __construct()
+    {
+        parent::__construct(RUNTIME_DIR, RUNTIME_FILE);
+    }
+}
+
 function main(array $fstrim_mounts, array $zpools): void
 {
+    if (!UnraidStatus::array_started()) {
+        return;
+    }
+    try {
+        $runtime = new RuntimeState();
+    } catch (RuntimeStateException) {
+        return;
+    }
     foreach ($fstrim_mounts as $mountpoint) {
         if (!system_command('fstrim -v ' . escapeshellarg($mountpoint))) {
             logger('Cannot run fstrim on mountpoint: ' . $mountpoint, LOG_LEVEL::ERROR);
@@ -27,6 +46,7 @@ function main(array $fstrim_mounts, array $zpools): void
             logger('Cannot run zpool trim on zpool: ' . $zpool, LOG_LEVEL::ERROR);
         }
     }
+    $runtime->commit();
 }
 
 main($fstrim_mounts, $zpools);
