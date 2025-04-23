2d7250a77de7984e3661a9d8a8f7e684156836fe
KarlKKang <karl_kai.kang@icloud.com>
2025-04-22 22:25:52 -0400

    Format the kjhybb-nas-rsync-server start script

--- containers/kjhybb-nas-rsync-server/start.sh
+++ containers/kjhybb-nas-rsync-server/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker run -d --name kjhybb-nas-rsync-server -p 8022:22 --restart always \
--v /mnt/user/kjhybb-nas:/data \
--v /mnt/user/appdata/kjhybb-nas-rsync-server:/config:ro \
--e PASSWORD_FILE=/config/password \
---memory 128m --memory-swap -1 \
-rsync-server
+    -v /mnt/user/kjhybb-nas:/data \
+    -v /mnt/user/appdata/kjhybb-nas-rsync-server:/config:ro \
+    -e PASSWORD_FILE=/config/password \
+    --memory 128m --memory-swap -1 \
+    rsync-server
