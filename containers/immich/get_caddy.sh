2ef36ae1a22ece41b795b96b528b355ebee87800
KarlKKang <karl_kai.kang@icloud.com>
2025-04-24 20:59:12 -0400

    Remove the `idempotency` query when downloading caddy

--- containers/immich/get_caddy.sh
+++ containers/immich/get_caddy.sh
@@ -3,5 +3,5 @@
 set -e
 cd "$(dirname "$0")"
 
-curl -L -o caddy 'https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fcaddy-dns%2Fcloudflare&idempotency=60693466075896'
+curl -L -o caddy 'https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fcaddy-dns%2Fcloudflare'
 chmod +x caddy
