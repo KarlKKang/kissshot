d1aad60a97943d54d408756811556e655cec8992
KarlKKang <karl_kai.kang@icloud.com>
2025-04-28 11:00:41 -0400

    Adjust bzroot compression settings

--- bzroot/pack.sh
+++ bzroot/pack.sh
@@ -4,7 +4,7 @@ set -e
 cd "$(dirname "$0")"
 
 cd ./root
-find . | cpio -o -H newc | xz --check=crc32 --ia64 --lzma2=preset=9e "--threads=$(nproc --all)" >../bzroot.part
+find . | cpio -o -H newc | xz --check=crc32 --x86 --lzma2=preset=9e >../bzroot.part
 cd ..
 cat ./microcode ./bzroot.part >./bzroot
 sha256sum ./bzroot | awk '{print $1}' >./bzroot.sha256
