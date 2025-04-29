143a5238d22f193a80b3db2e900efc3738a535e4
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 13:17:23 -0400

    Log patched files

--- patches/patch_running.sh
+++ patches/patch_running.sh
@@ -3,8 +3,17 @@
 set -e
 cd "$(dirname "$0")"
 
-cmp --silent ./rc.S /usr/local/etc/rc.d/rc.S || cat ./rc.S >/usr/local/etc/rc.d/rc.S
-cmp --silent ./rc.6 /usr/local/etc/rc.d/rc.6 || cat ./rc.6 >/usr/local/etc/rc.d/rc.6
-cmp --silent ./flash_backup /usr/local/emhttp/plugins/dynamix/scripts/flash_backup || cat ./flash_backup >/usr/local/emhttp/plugins/dynamix/scripts/flash_backup
+cmp --silent ./rc.S /usr/local/etc/rc.d/rc.S || {
+    echo "Patching rc.S..."
+    cat ./rc.S >/usr/local/etc/rc.d/rc.S
+}
+cmp --silent ./rc.6 /usr/local/etc/rc.d/rc.6 || {
+    echo "Patching rc.6..."
+    cat ./rc.6 >/usr/local/etc/rc.d/rc.6
+}
+cmp --silent ./flash_backup /usr/local/emhttp/plugins/dynamix/scripts/flash_backup || {
+    echo "Patching flash_backup..."
+    cat ./flash_backup >/usr/local/emhttp/plugins/dynamix/scripts/flash_backup
+}
 
 echo "All files patched successfully."
