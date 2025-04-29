04f86f8c408669db595fb4ea891c113e31d18e70
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 13:10:45 -0400

    Add the flash_backup script to the patch

--- patches/bzfirmware/patch.sh
+++ patches/bzfirmware/patch.sh
@@ -5,5 +5,6 @@ cd "$(dirname "$0")"
 
 cat ../rc.S >./usr/local/etc/rc.d/rc.S
 cat ../rc.6 >./usr/local/etc/rc.d/rc.6
+cat ../flash_backup >./usr/local/emhttp/plugins/dynamix/scripts/flash_backup
 
 echo "All files patched successfully."
