cc2bf5f5eca4732d67938c96bb940bea54f3dbb8
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 13:15:43 -0400

    Only patch running version if they differ

--- patches/patch_running.sh
+++ patches/patch_running.sh
@@ -3,8 +3,8 @@
 set -e
 cd "$(dirname "$0")"
 
-cat ./rc.S >/usr/local/etc/rc.d/rc.S
-cat ./rc.6 >/usr/local/etc/rc.d/rc.6
-cat ./flash_backup >/usr/local/emhttp/plugins/dynamix/scripts/flash_backup
+cmp --silent ./rc.S /usr/local/etc/rc.d/rc.S || cat ./rc.S >/usr/local/etc/rc.d/rc.S
+cmp --silent ./rc.6 /usr/local/etc/rc.d/rc.6 || cat ./rc.6 >/usr/local/etc/rc.d/rc.6
+cmp --silent ./flash_backup /usr/local/emhttp/plugins/dynamix/scripts/flash_backup || cat ./flash_backup >/usr/local/emhttp/plugins/dynamix/scripts/flash_backup
 
 echo "All files patched successfully."
