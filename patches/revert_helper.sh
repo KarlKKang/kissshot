5d850437653fef8abd91b3c46be8a626e8ea41a8
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 09:07:18 -0400

    Check the existence of old files before reverting

--- patches/revert_helper.sh
+++ patches/revert_helper.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ ! -f /boot/$BZFILE.old ] || [ ! -f /boot/$BZFILE.sha256.old ]; then
+    echo "No old files found. Nothing to revert."
+    exit 0
+fi
+
 mv -f /boot/$BZFILE.old /boot/$BZFILE
 mv -f /boot/$BZFILE.sha256.old /boot/$BZFILE.sha256
 
