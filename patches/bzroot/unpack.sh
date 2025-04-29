b52004909cd78e56cc14e6219a526c28f565ad6f
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 07:11:55 -0400

    Use a variable to hold the file name in the bzroot unpack script

--- patches/bzroot/unpack.sh
+++ patches/bzroot/unpack.sh
@@ -3,17 +3,19 @@
 set -e
 cd "$(dirname "$0")"
 
-cp /boot/bzroot ./bzroot
-HASH1=$(sha256sum ./bzroot)
-HASH2=$(cat /boot/bzroot.sha256)
+BZFILE="bzroot"
+
+cp /boot/$BZFILE ./$BZFILE
+HASH1=$(sha256sum ./$BZFILE)
+HASH2=$(cat /boot/$BZFILE.sha256)
 if [ "${HASH1:0:64}" != "${HASH2:0:64}" ]; then
-    echo "bzroot hash mismatch"
+    echo "$BZFILE hash mismatch"
     exit 1
 fi
 
-dd if=./bzroot bs=512 "count=$(cpio -ivt -H newc <./bzroot 2>&1 >/dev/null | awk '{print $1}')" of=./microcode
+dd if=./$BZFILE bs=512 "count=$(cpio -ivt -H newc <./$BZFILE 2>&1 >/dev/null | awk '{print $1}')" of=./microcode
 mkdir -p ./root
 cd ./root
-dd if=../bzroot bs=512 "skip=$(cpio -ivt -H newc <../bzroot 2>&1 >/dev/null | awk '{print $1}')" | xzcat | cpio -i -d -H newc --no-absolute-filenames
+dd if=../$BZFILE bs=512 "skip=$(cpio -ivt -H newc <../$BZFILE 2>&1 >/dev/null | awk '{print $1}')" | xzcat | cpio -i -d -H newc --no-absolute-filenames
 
 echo "All files unpacked successfully."
