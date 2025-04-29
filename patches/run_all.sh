1b33ba085ae7143d2fa4d25bb26e625313b92bef
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 08:34:47 -0400

    Use a universal run_all script to patch the bzfiles

---
+++ patches/run_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <BZFILE>"
+    exit 1
+fi
+SCRIPT_DIR="$(dirname "$0")/$1"
+if [[ ! -d "$SCRIPT_DIR" ]]; then
+    echo "Error: Directory $SCRIPT_DIR does not exist."
+    exit 1
+fi
+sh "$SCRIPT_DIR/unpack.sh"
+sh "$SCRIPT_DIR/patch.sh"
+sh "$SCRIPT_DIR/pack.sh"
+sh "$SCRIPT_DIR/deploy.sh"
+sh "$SCRIPT_DIR/cleanup.sh"
