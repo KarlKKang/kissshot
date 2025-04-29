f2e769dc09af5b89d25d388c426c26ba69a43a9f
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 12:54:18 -0400

    Don't deploy patches if old an package is found

--- patches/deploy_helper.sh
+++ patches/deploy_helper.sh
@@ -2,25 +2,13 @@
 
 set -e
 
-if [ -f /boot/$BZFILE.old ] && [ -f /boot/$BZFILE.sha256.old ]; then
+if [ -f /boot/$BZFILE.old ] || [ -f /boot/$BZFILE.sha256.old ]; then
     echo "A previous version of $BZFILE already exists."
-    echo "Do you want to discard the current version (y) or discard the previous version (n)?"
-    read -r choice
-    if [ "$choice" = "y" ] || [ "$choice" = "Y" ]; then
-        rm -f /boot/$BZFILE
-        rm -f /boot/$BZFILE.sha256
-    elif [ "$choice" = "n" ] || [ "$choice" = "N" ]; then
-        mv -f /boot/$BZFILE /boot/$BZFILE.old
-        mv -f /boot/$BZFILE.sha256 /boot/$BZFILE.sha256.old
-    else
-        echo "Invalid choice. Exiting."
-        exit 1
-    fi
-else
-    mv /boot/$BZFILE /boot/$BZFILE.old
-    mv /boot/$BZFILE.sha256 /boot/$BZFILE.sha256.old
+    exit 1
 fi
 
+mv /boot/$BZFILE /boot/$BZFILE.old
+mv /boot/$BZFILE.sha256 /boot/$BZFILE.sha256.old
 mv ./$BZFILE /boot/$BZFILE
 mv ./$BZFILE.sha256 /boot/$BZFILE.sha256
 
