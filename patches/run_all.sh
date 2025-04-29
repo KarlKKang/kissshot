2dcf5a3fb6012699aa8889b9c2e1174920c6d2bd
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 12:43:13 -0400

    Revert before patching the bzfiles

--- patches/run_all.sh
+++ patches/run_all.sh
@@ -11,6 +11,7 @@ if [[ ! -d "$SCRIPT_DIR" ]]; then
     echo "Error: Directory $SCRIPT_DIR does not exist."
     exit 1
 fi
+sh "$SCRIPT_DIR/revert.sh"
 sh "$SCRIPT_DIR/unpack.sh"
 sh "$SCRIPT_DIR/patch.sh"
 sh "$SCRIPT_DIR/pack.sh"
