a3c9f73be133801de5a8c13c1ecdf2eae9fd1640
KarlKKang <karl_kai.kang@icloud.com>
2025-04-29 10:48:47 -0400

    Add hourly health check for ZFS and btrfs.

--- plugins/kissshot-plugin/source/cron/kissshot-plugin.hourly.sh
+++ plugins/kissshot-plugin/source/cron/kissshot-plugin.hourly.sh
@@ -6,3 +6,10 @@ if [ $exit_code -ne 0 ]; then
     logger -t zfs-auto-snapshot "[error] Script exited with error code $exit_code"
     /usr/local/emhttp/webGui/scripts/notify -e "ZFS Auto Snapshot" -d "Script exited with error code $exit_code" -i "alert"
 fi
+
+php "/usr/local/emhttp/plugins/kissshot-plugin/health-check.php"
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    logger -t health-check "[error] Script exited with error code $exit_code"
+    /usr/local/emhttp/webGui/scripts/notify -e "Health Check" -d "Script exited with error code $exit_code" -i "alert"
+fi
---
+++ plugins/kissshot-plugin/source/health-check.php
@@ -0,0 +1,72 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+const SCRIPT_NAME = 'health-check';
+const NOTIFICATION_TITLE = 'Health Check';
+
+require __DIR__ . '/helper.php';
+
+$zpools = [
+    'kokorowatari',
+    'golden_chocolate',
+];
+$btrfs_mounts = [
+    '/boot/config'
+];
+
+function check_zpool(string $zpool): void
+{
+    $output = [];
+    if (!system_command('zpool status -x -v ' . escapeshellarg($zpool), $output)) {
+        logger('Cannot get zpool status for: ' . $zpool, LOG_LEVEL::ERROR);
+        return;
+    }
+    $healthy = true;
+    foreach ($output as $line) {
+        if (empty($line)) {
+            continue;
+        }
+        if ($line !== "pool '" . $zpool . "' is healthy") {
+            logger($line);
+            $healthy = false;
+        }
+    }
+    if (!$healthy) {
+        logger('ZFS errors detected on: ' . $zpool, LOG_LEVEL::ERROR);
+    }
+}
+
+function check_btrfs(string $mountpoint): void
+{
+    $output = [];
+    try {
+        exec('btrfs dev stats -c ' . escapeshellarg($mountpoint), $output, $retval);
+    } catch (ValueError $e) {
+        logger($e->getMessage());
+        logger('Cannot get btrfs status for: ' . $mountpoint, LOG_LEVEL::ERROR);
+        return;
+    }
+    if ($retval !== 0) {
+        foreach ($output as $line) {
+            if (empty($line)) {
+                continue;
+            }
+            logger($line);
+        }
+        logger('Btrfs errors detected on: ' . $mountpoint, LOG_LEVEL::ERROR);
+    }
+}
+
+function main(array $zpools, array $btrfs_mounts): void
+{
+    foreach ($zpools as $zpool) {
+        check_zpool($zpool);
+    }
+    foreach ($btrfs_mounts as $mountpoint) {
+        check_btrfs($mountpoint);
+    }
+}
+
+main($zpools, $btrfs_mounts);
