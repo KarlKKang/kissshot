7704988ae00e5ef598803ac980f869268248ab1f
KarlKKang <karl_kai.kang@icloud.com>
2025-05-03 01:55:36 -0400

    Move all patch sources into a separate directory

--- patches/bzfirmware/patch.sh
+++ patches/bzfirmware/patch.sh
@@ -3,9 +3,11 @@
 set -e
 cd "$(dirname "$0")"
 
-cat ../rc.S >./usr/local/etc/rc.d/rc.S
-cat ../rc.6 >./usr/local/etc/rc.d/rc.6
-cat ../rc.runlog >./usr/local/etc/rc.d/rc.runlog
-cat ../flash_backup >./usr/local/emhttp/plugins/dynamix/scripts/flash_backup
+SRC_DIR="../src"
+
+cat $SRC_DIR/rc.S >./usr/local/etc/rc.d/rc.S
+cat $SRC_DIR/rc.6 >./usr/local/etc/rc.d/rc.6
+cat $SRC_DIR/rc.runlog >./usr/local/etc/rc.d/rc.runlog
+cat $SRC_DIR/flash_backup >./usr/local/emhttp/plugins/dynamix/scripts/flash_backup
 
 echo "All files patched successfully."
