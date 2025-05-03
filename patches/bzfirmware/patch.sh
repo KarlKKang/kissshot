c5daa5f5da04efe4412a7d338be3fde31afaf457
KarlKKang <karl_kai.kang@icloud.com>
2025-05-03 01:33:44 -0400

    Apply the runlog patch

--- patches/bzfirmware/patch.sh
+++ patches/bzfirmware/patch.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$0")"
 
 cat ../rc.S >./usr/local/etc/rc.d/rc.S
 cat ../rc.6 >./usr/local/etc/rc.d/rc.6
+cat ../rc.runlog >./usr/local/etc/rc.d/rc.runlog
 cat ../flash_backup >./usr/local/emhttp/plugins/dynamix/scripts/flash_backup
 
 echo "All files patched successfully."
--- patches/patch_running.sh
+++ patches/patch_running.sh
@@ -3,17 +3,22 @@
 set -e
 cd "$(dirname "$0")"
 
-cmp --silent ./rc.S /usr/local/etc/rc.d/rc.S || {
-    echo "Patching rc.S..."
-    cat ./rc.S >/usr/local/etc/rc.d/rc.S
+apply_patch() {
+    local name="$1"
+    local src="$2"
+    local dest="$3"
+
+    if cmp --silent "$src" "$dest"; then
+        echo "No changes to $name."
+    else
+        echo "Patching $name..."
+        cat "$src" >"$dest"
+    fi
 }
-cmp --silent ./rc.6 /usr/local/etc/rc.d/rc.6 || {
-    echo "Patching rc.6..."
-    cat ./rc.6 >/usr/local/etc/rc.d/rc.6
-}
-cmp --silent ./flash_backup /usr/local/emhttp/plugins/dynamix/scripts/flash_backup || {
-    echo "Patching flash_backup..."
-    cat ./flash_backup >/usr/local/emhttp/plugins/dynamix/scripts/flash_backup
-}
+
+apply_patch rc.S ./rc.S /usr/local/etc/rc.d/rc.S
+apply_patch rc.6 ./rc.6 /usr/local/etc/rc.d/rc.6
+apply_patch rc.runlog ./rc.runlog /usr/local/etc/rc.d/rc.runlog
+apply_patch flash_backup ./flash_backup /usr/local/emhttp/plugins/dynamix/scripts/flash_backup
 
 echo "All files patched successfully."
