7af8831d83bbd77e299c9e160d124c3e9a777e3a
KarlKKang <karl_kai.kang@icloud.com>
2025-05-04 15:32:49 -0400

    Add a comment to the rsync-server container script

--- containers/kjhybb-nas-rsync-server/start.sh
+++ containers/kjhybb-nas-rsync-server/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Source: https://github.com/KarlKKang/rsync-server
+# Used as a backup destination for Synology Hyper Backup
+
 docker run -d --name kjhybb-nas-rsync-server -p 8022:22 --restart always \
     -v /mnt/user/kjhybb-nas:/data \
     -v /mnt/user/appdata/kjhybb-nas-rsync-server:/config:ro \
