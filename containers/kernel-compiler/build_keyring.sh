275516b084d3a982f3c766362903ff07eafe42f1
KarlKKang <karl_kai.kang@icloud.com>
2025-05-05 01:43:52 -0400

    Use the kernel-compiler container itself  to build the keyring volume

--- containers/kernel-compiler/build_keyring.sh
+++ containers/kernel-compiler/build_keyring.sh
@@ -2,7 +2,7 @@
 
 set -e 
 
-alias gpg2="docker run -it --rm --name gpg2 -v kernel-compiler-keyring:/root/.gnupg gpg2"
+alias gpg2="docker run -it --rm --name gpg2 -v kernel-compiler-keyring:/root/.gnupg kernel-compiler gpg2"
 
 gpg2 --keyserver keyserver.ubuntu.com --recv-keys 647F28654894E3BD457199BE38DBBDC86092693E
 gpg2 --tofu-policy good 647F28654894E3BD457199BE38DBBDC86092693E
