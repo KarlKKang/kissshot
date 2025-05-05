8be03f054cce7ba1916fa5eb33edeee2dcf0fc17
KarlKKang <karl_kai.kang@icloud.com>
2025-05-05 01:25:21 -0400

    patch: Use environment variable to locate the src folder.

--- patches/kernel/patch.sh
+++ patches/kernel/patch.sh
@@ -18,5 +18,5 @@ cd "$(dirname "$0")"
 
 PARENT_DIR="$(realpath ..)"
 
-docker run -it --rm --name kernel-compiler -v kernel-compiler-keyring:/root/.gnupg -v "$PARENT_DIR/bzfirmware/usr/src/linux-6.6.78-Unraid:/data/patches" -v "${PWD}:/data/output"  \
+docker run -it --rm --name kernel-compiler -v kernel-compiler-keyring:/root/.gnupg -v "$PARENT_DIR/bzfirmware/usr/src/linux-${FULL_VER}-Unraid:/data/patches" -v "${PWD}:/data/output"  \
     -e MAJOR_VER="$MAJOR_VER" -e FULL_VER="$FULL_VER" -e PATCH_DIR=/data/patches -e ZFS_VER="$ZFS_VER" -e OUT_DIR=/data/output kernel-compiler
