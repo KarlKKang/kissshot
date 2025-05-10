12e1146e1c086172fe31470c667448fa506acee8
KarlKKang <karl_kai.kang@icloud.com>
2025-05-10 16:00:12 -0400

    kernel-compiler: Fix keyring build script

--- containers/kernel-compiler/build_keyring.sh
+++ containers/kernel-compiler/build_keyring.sh
@@ -2,7 +2,9 @@
 
 set -e 
 
-alias gpg2="docker run -it --rm --name gpg2 -v kernel-compiler-keyring:/root/.gnupg kernel-compiler gpg2"
+gpg2() {
+    docker run -it --rm --name gpg2 -v kernel-compiler-keyring:/root/.gnupg kernel-compiler gpg2 "$@"
+}
 
 gpg2 --keyserver keyserver.ubuntu.com --recv-keys 647F28654894E3BD457199BE38DBBDC86092693E
 gpg2 --tofu-policy good 647F28654894E3BD457199BE38DBBDC86092693E
@@ -12,3 +14,5 @@ gpg2 --tofu-policy good C77B9667
 
 gpg2 --keyserver pgp.mit.edu --recv D4598027
 gpg2 --tofu-policy good D4598027
+
+echo "Keys loaded successfully."
