851b0fc30ff5e69e3cffad8c1306cbd1c6b5b0ae
KarlKKang <karl_kai.kang@icloud.com>
2025-05-14 14:53:19 -0400

    Mount only the files necessary for docker compose of the immich service

--- containers/immich/docker-compose.sh
+++ containers/immich/docker-compose.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 
 docker run -it --rm --name docker-compose --network none \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /mnt/user/appdata/immich:/mnt/user/appdata/immich:ro \
+    -v /mnt/user/appdata/immich/caddy.env:/mnt/user/appdata/immich/caddy.env:ro \
     -v "$PWD:$PWD:ro" \
     -w="$PWD" \
     docker compose "$@"
