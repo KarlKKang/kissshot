dcfb7d8b70d92f539f86cdcd8f4af4f143a5cc4b
KarlKKang <karl_kai.kang@icloud.com>
2025-05-22 08:46:33 -0400

    patch: change the mountpoint to `/mnt/root` in the move_config script

--- patches/tools/move_config.sh
+++ patches/tools/move_config.sh
@@ -2,19 +2,26 @@
 
 set -e
 
-if [ -d "/boot_extra/config" ]; then
-    if ! rmdir /boot_extra/config; then
+MNT_DIR="/mnt/root"
+
+if ! mountpoint -q "$MNT_DIR"; then
+    echo "$MNT_DIR is not a mountpoint. Please mount it first."
+    exit 1
+fi
+
+if [ -d "$MNT_DIR/config" ]; then
+    if ! rmdir "$MNT_DIR/config"; then
         echo "New config already exists. Exiting."
         exit 1
     fi
 fi
 
-mkdir /boot_extra/config
-rsync -a /boot/config/ /boot_extra/config/
+mkdir "$MNT_DIR/config"
+rsync -a /boot/config/ "$MNT_DIR/config/"
 if mountpoint -q /boot/config; then
     umount /boot/config
 else
     mv /boot/config /boot/config.old
     mkdir /boot/config
 fi
-mount --bind /boot_extra/config /boot/config
+mount --bind "$MNT_DIR/config" /boot/config
