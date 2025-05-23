0f1a9343371cabf360f03fb237565e0ac8b8684a
KarlKKang <karl_kai.kang@icloud.com>
2025-05-22 22:11:41 -0400

    Don't cross filesystem boundaries when finding nocow files, and find only file and directories

--- find_nocow.sh
+++ find_nocow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # btrfs implicitly disable checksum on files with the C attribute
-find . ! -type l -exec lsattr -d {} + | sed -n 's/^[^ ]*C[^ ]* //p'
+find . -xdev -type f,d -exec lsattr -d {} + | sed -n 's/^[^ ]*C[^ ]* //p'
