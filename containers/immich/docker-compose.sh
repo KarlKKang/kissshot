4bce5f34ca29830c2a18c414881fb168f596a874
KarlKKang <karl_kai.kang@icloud.com>
2025-05-24 17:44:36 -0400

    Move caddy data and config directories to appdata share, as they contain sensitive information

--- containers/immich/docker-compose.sh
+++ containers/immich/docker-compose.sh
@@ -10,7 +10,7 @@ fi
 
 docker run -it --rm --name docker-compose --network none \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /mnt/user/appdata/immich/caddy.env:/mnt/user/appdata/immich/caddy.env:ro \
+    -v /mnt/user/appdata/immich/caddy/.env:/mnt/user/appdata/immich/caddy/.env:ro \
     -v "$PWD:$PWD:ro" \
     -w="$PWD" \
     docker compose "$@"
