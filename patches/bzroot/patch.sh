76ec7f16f577f48589f0b579ab2575f5b441b10a
KarlKKang <karl_kai.kang@icloud.com>
2025-05-25 10:27:48 -0400

    patch: include the ZFS options in the initramfs

--- patches/bzroot/patch.sh
+++ patches/bzroot/patch.sh
@@ -9,6 +9,7 @@ if [ ! -d ../bzfirmware/usr/lib64 ]; then
 fi
 
 cat ../src/rc.S >./root/etc/rc.d/rc.S
+
 mv -i ../bzfirmware/usr/lib64/liblzo2.so* ./root/lib64
 mv -i ../bzfirmware/usr/lib64/libunwind.so* ./root/lib64
 mv -i ../bzfirmware/usr/lib64/libgcc_s.so* ./root/lib64
@@ -16,6 +17,10 @@ mv -i ../bzfirmware/usr/sbin/zfs ./root/sbin
 mv -i ../bzfirmware/usr/sbin/zpool ./root/sbin
 ln -si ../../sbin/zfs ../bzfirmware/usr/sbin
 ln -si ../../sbin/zpool ../bzfirmware/usr/sbin
+
+mkdir -p ./root/etc/modprobe.d
+cat ../../boot/config/modprobe.d/zfs.conf >./root/etc/modprobe.d/zfs.conf
+
 rm ./root/init
 cat ../src/init >./root/init
 chown root:root ./root/init
