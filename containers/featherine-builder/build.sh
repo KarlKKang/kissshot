eb7c18ce7861e024fd5e9661ebe6abbc68108162
KarlKKang <karl_kai.kang@icloud.com>
2025-06-20 19:14:18 -0400

    Add python to the featherine builder image

--- containers/featherine-builder/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e 
-cd "$(dirname "$0")"
-
-docker build -t featherine-builder --pull .
--- containers/featherine-builder/run.sh
+++ containers/featherine-builder/run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-docker run -it --rm --name featherine-builder --network host -v /mnt/user/appdata/featherine-builder/aws_credentials:/root/.aws/credentials:ro -v featherine-src:/root/src -w /root/src featherine-builder
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <SRC_DIR>"
+    exit 1
+fi
+SRC_DIR=$(realpath "$1")
+
+cd "$(dirname "$0")"
+
+docker build -t featherine-builder --pull .
+docker run -it --rm --name featherine-builder --network host -v /mnt/user/appdata/featherine-builder/aws_credentials:/root/.aws/credentials:ro -v "$SRC_DIR":/root/src -w /root/src featherine-builder
