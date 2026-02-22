c034f740779807882700fd797e0c28032f2daea8
KarlKKang <karl_kai.kang@icloud.com>
2026-02-22 15:30:19 +0900

    plugin: Make cron commands run in the background. UnRAID uses Dillon's cron implementation which waits for the previous run to finish before the next scheduled execution. However, we handle the lockings by ourselves in the plugin, thus this behavior is unwanted.

--- plugins/kissshot-plugin/source/cron/kissshot-plugin.daily.sh
+++ plugins/kissshot-plugin/source/cron/kissshot-plugin.daily.sh
@@ -2,5 +2,7 @@
 
 PHP_RUN="/usr/local/emhttp/plugins/kissshot-plugin/run_php.sh"
 
-SCRIPT_NAME="recycle-clean" NOTIFICATION_TITLE="Recycle Clean" $PHP_RUN
-SCRIPT_NAME="trim" NOTIFICATION_TITLE="TRIM" $PHP_RUN
+{
+    SCRIPT_NAME="recycle-clean" NOTIFICATION_TITLE="Recycle Clean" $PHP_RUN
+    SCRIPT_NAME="trim" NOTIFICATION_TITLE="TRIM" $PHP_RUN
+} &
--- plugins/kissshot-plugin/source/cron/kissshot-plugin.hourly.sh
+++ plugins/kissshot-plugin/source/cron/kissshot-plugin.hourly.sh
@@ -2,5 +2,5 @@
 
 PHP_RUN="/usr/local/emhttp/plugins/kissshot-plugin/run_php.sh"
 
-SCRIPT_NAME="zfs-backup" NOTIFICATION_TITLE="ZFS Backup" $PHP_RUN
-SCRIPT_NAME="health-check" NOTIFICATION_TITLE="Health Check" $PHP_RUN
+SCRIPT_NAME="zfs-backup" NOTIFICATION_TITLE="ZFS Backup" $PHP_RUN &
+SCRIPT_NAME="health-check" NOTIFICATION_TITLE="Health Check" $PHP_RUN &
